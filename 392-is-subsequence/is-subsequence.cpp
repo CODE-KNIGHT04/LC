8a70e5b793fc9b4f55435856fe199f06ba42ec41
CODE-KNIGHT04 <24ucc022@lnmiit.ac.in>
2026-07-14 04:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (9.08%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n = s.length(), m = t.length();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return solve(n, m, s, t, dp);
+        int i = 0, j = 0; // i walks s, j walks t
+        while(i < s.length() && j < t.length()) {
+            if(s[i] == t[j])
+                i++;       // matched, advance s pointer
+            j++;           // always advance t pointer
+        }
+        return i == s.length(); // did we match all of s?
 }
-int solve(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if(i == 0)
-            return 1; // empty prefix of s is always a subsequence
-        if(j == 0)
-            return 0; // s still has chars left but t ran out
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = solve(i, j-1, s, t, dp); // skip t[j-1]
-
-        if(s[i-1] == t[j-1])
-            ans = ans || solve(i-1, j-1, s, t, dp); // also try matching here
-
-        return dp[i][j] = ans;
-    }
 };
