df8c09fa5dce6e27de034d6c8566b882d31375b6
CODE-KNIGHT04 <24ucc022@lnmiit.ac.in>
2026-07-21 00:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (22.65%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.c
@@ -0,0 +1,30 @@
+int getValue(char c) {
+    switch (c) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+    }
+    return 0;
+}
+
+int romanToInt(char* s) {
+    int total = 0;
+    int n = strlen(s);
+    
+    for (int i = 0; i < n; i++) {
+        int curr = getValue(s[i]);
+        int next = (i + 1 < n) ? getValue(s[i + 1]) : 0;
+        
+        if (curr < next) {
+            total -= curr;
+        } else {
+            total += curr;
+        }
+    }
+    
+    return total;
+}
